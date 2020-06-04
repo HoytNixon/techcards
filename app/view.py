45b5cec2949b1a1b7d88461e66282025f828eb2b
hotnixon <perepciukdima@yandex.ru>
2020-06-04 17:43:38 +0300

    adding cookies

--- app/view.py
+++ app/view.py
@@ -1,12 +1,35 @@
 from .import app
-from flask import render_template, request, url_for, redirect, flash
+from flask import render_template, request, url_for, redirect, flash, g, session
 from .forms import Wform
 from .remake_xls import remake_shablon
 from .emailxls import send_techcard
 import os
+
+
+@app.before_request
+def before_request():
+	if 'shifr' in session:
+		form= Wform()
+		form.shifr.data = session['shifr']
+		form.name.data = session['name']
+		form.diametr.data = session['diametr']
+		form.tolshina.data = session['tolshina']
+		form.number.data = session['number']
+		form.typ.data = session['typ']
+		form.quality.data = session['quality']
+		g.form = form
+		session.pop('shifr', None)
+		session.pop('name', None)
+		session.pop('diametr', None)
+		session.pop('tolshina', None)
+		session.pop('number', None)
+		session.pop('typ', None)
+		session.pop('quality', None)
+	else:
+		g.form = Wform()
 @app.route('/', methods =['GET', 'POST'])
 def index():
-	form = Wform()
+	form=g.form
 	if form.validate_on_submit():
 		shifr = form.shifr.data
 		name = form.name.data
@@ -16,7 +39,7 @@ def index():
 		typ = form.typ.data
 		quality = form.quality.data
 		remake_shablon(shifr, name, diametr, tolshina, typ, number, quality)
-
+		flash(f'Техкарта c шифром "{shifr}" отправлена')
 		filename = 'static/' + shifr + '.xlsx'
 		##  отправить файл по почте ##
 		#send_techcard(app.config['MAIL_RECIPIENT'], name, filename)
@@ -24,13 +47,13 @@ def index():
 		## Удалить после отправки ##
 		#path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
 		#os.remove(path)
-
-		form.shifr.data = shifr
-		form.name.data = name
-		form.diametr.data = diametr
-		form.tolshina.data = tolshina
-		form.number.data = number
-		form.typ.data = typ
-		form.quality.data = quality
-		return render_template('index.html', form=form)	
+		session['shifr'] = shifr
+		session['name'] = name
+		session['diametr']= diametr
+		session['tolshina'] = tolshina
+		session['number'] = number
+		session['typ'] = typ
+		session['quality'] = quality
+		return redirect(url_for('index'))	
 	return render_template('index.html', form=form)
+	
